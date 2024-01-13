da2017b62fac3f83f4c7d5f13bfd2dca27075a30
Mark Jungeblut <mark.jungeblut.development@gmail.com>
2024-01-13 15:10:25 +0100

    designed list item for exercises

--- lib/feature/training_plan/new_training_plan.dart
+++ lib/feature/training_plan/new_training_plan.dart
@@ -65,9 +65,58 @@ class NewTrainingPlan extends StatelessWidget {
                           final Iterable<Exercise> exercisesOfGroup = exercises.where((element) => element.exerciseGroup.id == exerciseGroups[index].id);
 
                           return ExpansionTile(
+                            initiallyExpanded: true,
                             title: Text(exerciseGroups[index].name),
                             children: exercisesOfGroup.map((exercise) {
-                              return Text(exercise.name, textAlign: TextAlign.left);
+                              return InkWell(
+                                onTap: () {},
+                                child: Container(
+                                    height: 60,
+                                    margin: const EdgeInsets.only(left: 20, right: 20),
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(color: Colors.grey),
+                                      ),
+                                    ),
+                                    child: Container(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            width: 60,
+                                            margin: const EdgeInsets.all(5),
+                                            decoration: const BoxDecoration(
+                                                image: DecorationImage(
+                                                  fit: BoxFit.cover,
+                                                  image: NetworkImage("https://as2.ftcdn.net/v2/jpg/01/79/81/77/1000_F_179817756_QzTocli57q9G6a1Oe7kJtoMS5dNMU8cl.jpg"),
+                                                )
+                                            ),
+                                          ),
+                                          Container(
+                                              margin: const EdgeInsets.only(left: 10, top: 10),
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                      exercise.name,
+                                                      textAlign: TextAlign.left,
+                                                      style: const TextStyle(
+                                                          fontWeight: FontWeight.bold
+                                                      )
+                                                  ),
+                                                  const Text(
+                                                      "foo",
+                                                      textAlign: TextAlign.left
+                                                  )
+                                                ],
+                                              )
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                ),
+                              );
                             }).toList(),
                           );
                         },
